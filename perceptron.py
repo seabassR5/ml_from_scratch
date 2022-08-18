48eb7fcaefe39c0fb352f8a6696d42cf8a741946
Sebastian Reyes <sebastian.reyes@oracle.com>
2022-08-17 18:50:43 -0700

    understand a bit more today

--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,73 @@
 import numpy as np
+# following a Youtube tutorial from Polycode
 
+# this will be our normalizing function phi(x)
 def sigmoid(x):
+    """ normalizing funciton that returns number between 0 and 1 """
     return 1 / (1 + np.exp(-x))
 
-print(sigmoid(0))
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+training_inputs = np.array(
+    [
+        [0,0,1],
+        [1,1,1],
+        [1,0,1],
+        [0,1,1]
+    ]
+)
+
+training_outputs = np.array([[0,1,1,0]]).T
+# not really 100 sure why we need these yet
+np.random.seed(1)
+synaptic_weights = 2 * np.random.random((3,1)) - 1 
+print(f'random staring weights\n{synaptic_weights}')
+
+for itter in range(20000):
+
+    input_layer = training_inputs
+
+    # forward prop
+    # sigmoid(x1*w1 + x2*w2 + ... + xn*wn))) where n is the number of input nodes 
+    outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+    # out put y-hat (our current best aproximation of the true value y)
+    error = training_outputs - outputs
+
+    #calculate how much we need to adjust the weights
+    adjustments = error * sigmoid_derivative(outputs)
+    # we need -- dot | for linear algebra to work, so we take a transpose
+    adjust_weight_by = np.dot(input_layer.T, adjustments)
+
+    synaptic_weights += adjust_weight_by
+
+print(f"synaptic weights after traingin\n {synaptic_weights}")
+
+# want the outputs to look like training output?
+print(f"output after training:\n{outputs}")
+
+# general training process
+
+# take inputs from training example and 
+# run them through our current formula to get the output
+# (forward propigation?)
+
+# calculate the error
+
+# adjust weights depending on the severity of the error
+
+# repeat 20k times
+
+# ERROR WEIGHTED DERIVATE
+# adjust weight by = error * input * phi'(output) 
+# error = output - actual output
+# input = 1 or 0
+
+# if the output was a large negative or positive then that indicates high confidence
+# if we look at the derivative of phi, phi', 
+# then we see large inputs give small gradients, 
+# thus we dont adjust those weights as much
+# conversley??
+# if the output was small, that means low confidence, 
+# and the gradient of phi at that point is larger, 
+# indicating that we need to make a larger adjustment
